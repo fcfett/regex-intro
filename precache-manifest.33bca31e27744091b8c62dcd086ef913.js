2a7c8b7090dc29c8fb0ae7c27b5cfb87b346d59a
Felipe Fett <felipe.fett@opngo.com.br>
2019-10-21 10:40:27 -0300

    Updates

---
+++ precache-manifest.33bca31e27744091b8c62dcd086ef913.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06aae7fd6542026db03e4228b1c529f2",
+    "url": "/regex-intro/index.html"
+  },
+  {
+    "revision": "82a4af878884432437ba",
+    "url": "/regex-intro/static/css/main.79d45bc7.chunk.css"
+  },
+  {
+    "revision": "96eb92f4c2a714e381d5",
+    "url": "/regex-intro/static/js/2.4a9855b0.chunk.js"
+  },
+  {
+    "revision": "82a4af878884432437ba",
+    "url": "/regex-intro/static/js/main.febce316.chunk.js"
+  },
+  {
+    "revision": "e7c42ccc0d5b6e18e94d",
+    "url": "/regex-intro/static/js/runtime-main.1815209c.js"
+  },
+  {
+    "revision": "2823aff5ff968e83df9288ee83d8a9ce",
+    "url": "/regex-intro/static/media/alldaylong.2823aff5.jpg"
+  },
+  {
+    "revision": "56c8ef7f5333f94ffdd7a0e5ddcb4475",
+    "url": "/regex-intro/static/media/angle-down.56c8ef7f.svg"
+  },
+  {
+    "revision": "65e3d2e336ec62abf877c27acb2040c2",
+    "url": "/regex-intro/static/media/band.65e3d2e3.jpg"
+  },
+  {
+    "revision": "60b54ee87044af598a440a6396e5224a",
+    "url": "/regex-intro/static/media/bqc.60b54ee8.jpg"
+  },
+  {
+    "revision": "d7049c6c4265e21257878a5cea77265f",
+    "url": "/regex-intro/static/media/codepen.d7049c6c.svg"
+  },
+  {
+    "revision": "47bfe40d24ed78036e3518156c81ef8b",
+    "url": "/regex-intro/static/media/craft.47bfe40d.jpg"
+  },
+  {
+    "revision": "6cad3fe6c721f81d289270946b697c16",
+    "url": "/regex-intro/static/media/facebook.6cad3fe6.svg"
+  },
+  {
+    "revision": "c09666fbe0e2db9d8ad6fc1d5ceaa1dc",
+    "url": "/regex-intro/static/media/family.c09666fb.jpg"
+  },
+  {
+    "revision": "93b8836ba3af4dc8c78211fcc102d87c",
+    "url": "/regex-intro/static/media/festival.93b8836b.jpg"
+  },
+  {
+    "revision": "9f994f8f7eda2599282d6af7d92e386a",
+    "url": "/regex-intro/static/media/fett.9f994f8f.png"
+  },
+  {
+    "revision": "bb1b752c5d1bd79fc4a4480352efc3e7",
+    "url": "/regex-intro/static/media/food.bb1b752c.jpg"
+  },
+  {
+    "revision": "58dbdb42cf711031a871a721b7f937fa",
+    "url": "/regex-intro/static/media/fun.58dbdb42.jpg"
+  },
+  {
+    "revision": "e372a0f0525d15af9b059dac9f4eb0b6",
+    "url": "/regex-intro/static/media/github.e372a0f0.svg"
+  },
+  {
+    "revision": "430299d438e525b9c4648388fc32b8d1",
+    "url": "/regex-intro/static/media/grupols.430299d4.jpg"
+  },
+  {
+    "revision": "2f41a8b8f3914eaa9341cff2767f5422",
+    "url": "/regex-intro/static/media/instagram.2f41a8b8.svg"
+  },
+  {
+    "revision": "456bbb6480ebb9619ad9620d20b85cd9",
+    "url": "/regex-intro/static/media/kleene.456bbb64.jpg"
+  },
+  {
+    "revision": "2892b1ccf684ff905dc7227f417f4b96",
+    "url": "/regex-intro/static/media/linkedin.2892b1cc.svg"
+  },
+  {
+    "revision": "de46d3b1e5db9608b77b2603757bb98e",
+    "url": "/regex-intro/static/media/memes.de46d3b1.jpg"
+  },
+  {
+    "revision": "7af39fc677698b3a147af1de432a18de",
+    "url": "/regex-intro/static/media/morebeer.7af39fc6.jpg"
+  },
+  {
+    "revision": "16bbd6c22df513c6d25f006cdfd96685",
+    "url": "/regex-intro/static/media/movies.16bbd6c2.jpg"
+  },
+  {
+    "revision": "573690a3eb62378bf23b320cfe3ca471",
+    "url": "/regex-intro/static/media/nopiracy.573690a3.png"
+  },
+  {
+    "revision": "1896d4dd02754ebe0c8da0e9cc53e35c",
+    "url": "/regex-intro/static/media/shows.1896d4dd.jpg"
+  },
+  {
+    "revision": "dae9d5beda4db4a4da61e10bac0745f8",
+    "url": "/regex-intro/static/media/stefanini.dae9d5be.jpg"
+  },
+  {
+    "revision": "e13a5cb1f643e66e4e2ff406ffb7bc87",
+    "url": "/regex-intro/static/media/thatsallfolks.e13a5cb1.jpg"
+  },
+  {
+    "revision": "436befe8397aeaf076fcc9631d4b2ce6",
+    "url": "/regex-intro/static/media/thompson.436befe8.jpg"
+  },
+  {
+    "revision": "c9f462f55192c64cfb83d0261ba8e2d0",
+    "url": "/regex-intro/static/media/vg8.c9f462f5.jpg"
+  },
+  {
+    "revision": "7574461a5c5a1f76027f773997bb9da7",
+    "url": "/regex-intro/static/media/woman.7574461a.png"
+  }
+]);
