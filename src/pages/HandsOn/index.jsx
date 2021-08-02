58ba56d142a200ef3a7f84b1b90c77b4aa11b32b
Felipe Fett <felipe.fett@hypeflame.com.br>
2021-08-02 18:39:54 -0300

    Updates pills text

--- src/pages/HandsOn/index.jsx
+++ src/pages/HandsOn/index.jsx
@@ -4,6 +4,6 @@ import PillSelector from '../../components/PillSelector';
 export default () => (
   <div id="page-18" className="page flex">
     <h2 className="txt-big font-serif">Bora tomar uma?</h2>
-    <PillSelector red="Data" blue="Email" />
+    <PillSelector red="Datas" blue="Emails" />
   </div>
 );
